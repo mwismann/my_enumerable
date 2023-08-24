cb3b1294487701e8fb784c8b0ef33759617385e8
iturres <emanueliturres.co@gmail.com>
2023-08-24 19:24:16 -0300

    feat: on 'my_list.rb' Added -each- method and:
    - Added -each- method to MyList class.
    - Instantiate MyList object and called methods to test.
    - print out the result of the -each- method.

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,4 @@
-require './my_enumerable'
+require "./my_enumerable"
 
 class MyList
   include MyEnumerable
@@ -8,4 +8,27 @@ class MyList
   def initialize(*list)
     @list = list
   end
+
+  def each(&block)
+    @list.each(&block)
+  end
 end
+
+list = MyList.new(1, 2, 3, 4)
+# ? => #<MyList: @list=[1, 2, 3, 4]>
+
+# * Test #all?
+p(list.all? { |e| e < 5 })
+# ? => true
+p(list.all? { |e| e > 5 })
+# ? => false
+
+# * Test #any?
+p(list.any? { |e| e == 2 })
+# ? => true
+p(list.any? { |e| e == 5 })
+# ? => false
+
+# * Test #filter
+p(list.filter(&:even?))
+# ? => [2, 4]
